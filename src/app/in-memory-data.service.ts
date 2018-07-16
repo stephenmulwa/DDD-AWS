25a4f04896d0ff6bbbce515fafa55e84a7df827e
stephenmulwa <stephen.mulwa@digitaldividedata.com>
2018-07-16 15:45:54 +0300

    ddd-commit

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,41 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+ 
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Stephen Odhiambo' },
+      { id: 12, name: 'Isaac Muteti Mwania' },
+      { id: 13, name: 'Paul Mwendwa Mulonzia' },
+      { id: 14, name: 'Stephen Kimani' },
+      { id: 15, name: 'Kelvin Kariuki' },
+      { id: 16, name: 'Stephen Ndusya Ndele' },
+      { id: 17, name: 'Nimoh Wairimu' },
+      { id: 18, name: 'Patrick Mukumbu' },
+      { id: 19, name: 'Maureen Chebet' },
+      { id: 20, name: 'Erick Soi' },
+      { id: 21, name: 'Damaris Sirengo' },
+      { id: 22, name: 'Cherin Onyango' },
+      { id: 23, name: 'John Mutunga Muia' },
+      { id: 24, name: 'Edward Onyango' },
+      { id: 25, name: 'Maurice Wainanina' },
+      { id: 26, name: 'Kevin Ochieng' },
+      { id: 27, name: 'Fredrick Sadia' },
+      { id: 28, name: 'Arnold Amollo' },
+      { id: 29, name: 'Stephen Mulwa' },
+      { id: 30, name: 'Michael Muinde' },
+      { id: 31, name: 'Anthony Ndegwa' },
+      { id: 32, name: 'Erick Ochieng' },
+      { id: 33, name: 'George Alusa' },
+      { id: 34, name: 'Concepter Sanya' },
+      { id: 35, name: 'Chrispus Kaloki' },
+      { id: 36, name: 'Jared Machuka' },
+      { id: 37, name: 'Sammy Juma' },
+      { id: 38, name: 'Lilian Omoke' },
+      { id: 39, name: 'Billy Nelson' },
+      { id: 40, name: 'Peter Onkendi' }
+
+
+    ];
+    return {heroes};
+  }
+}                        
