c7964d48bbe1cc12e0ad1f3031f8325f64acafdf
stephenmulwa <34302917+stephenmulwa@users.noreply.github.com>
2018-07-24 11:33:44 +0300

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        app-routingmodule.withRoutes([
+        routing.module.withRoutes([
           { path: '', component: AppComponent }
         ])
       ],
